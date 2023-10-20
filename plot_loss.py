406ed19430c40ebbde9752a57c5f2a8722e738b8
Pacific89 <philipp.faulhammer@gmail.com>
2023-10-20 12:51:40 +0200

    plot multiple trainings

--- plot_loss.py
+++ plot_loss.py
@@ -1,7 +1,7 @@
 import os
 from matplotlib import pyplot as plt
 import numpy as np
-
+import pandas as pd
 
 def plot_slurm_out():
 
@@ -31,24 +31,18 @@ def plot_slurm_out():
     plt.show()
 
 
-def plot_csv():
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import numpy as np
+def plot_csv(train_values, val_values, train_std, val_std):
 
-    train_csv_path = "train_log.csv"
-    val_csv_path = "valid_log.csv"
-
-    df = pd.read_csv(train_csv_path)
-    df_val = pd.read_csv(val_csv_path)
-    print(np.mean(df["iou_score"][450:]))
-    print(np.mean(df_val["iou_score"][450:]))
+    # print(np.mean(df["iou_score"][450:]))
+    # print(np.mean(df_val["iou_score"][450:]))
 
     plt.figure(figsize=(16,7))
     plt.rcParams.update({'font.size': 22})
-    plt.plot(np.arange(len(df["iou_score"])), df["iou_score"], label="train iou_score")
+    plt.plot(np.arange(len(train_values)), train_values, label="train iou_score", color="blue", alpha=0.99)
+    plt.errorbar(np.arange(len(train_values)), train_values, train_std, linestyle="none", marker="^", label="train iou_score", color="blue", alpha=0.3)
     plt.title("Optimization performance")
-    plt.plot(np.arange(len(df_val["iou_score"])), df_val["iou_score"], label="validation iou_score")
+    plt.plot(np.arange(len(val_values)), val_values, label="validation iou_score", color="orange", alpha=0.99)
+    plt.errorbar(np.arange(len(val_values)), val_values, val_std, linestyle="none", marker="^", label="val iou_score", color="orange", alpha=0.3)
     plt.xlabel("Epoch")
     plt.ylabel("IoU score")
     # ax.plot(np.arange(len(df["val_loss"])), df["val_loss"], label="val. loss")
@@ -60,5 +54,58 @@ def plot_csv():
     plt.show()
 
 
-plot_csv()
+# plot_csv()
 
+train_csv_path = "train_log.csv"
+val_csv_path = "valid_log.csv"
+
+df = pd.read_csv(train_csv_path)
+df_val = pd.read_csv(val_csv_path)
+
+import os
+import pandas as pd
+
+# Define the root directory where your experiment subdirectories are located
+root_directory = "C:\\Users\\faulhamm\\Documents\\Philipp\\Code\\cc-machine-learning\\results\\dataset_v2_42_images"
+
+# Initialize empty dataframes for train and validation logs
+train_logs_df = pd.DataFrame()
+valid_logs_df = pd.DataFrame()
+train_experiment_dfs = []
+val_experiment_dfs = []
+
+# Iterate through subdirectories
+for subdir in os.listdir(root_directory):
+    subdir_path = os.path.join(root_directory, subdir)
+
+    # Check if the path is a directory
+    if os.path.isdir(subdir_path):
+        # Look for train_log.csv and valid_log.csv files
+        train_log_path = os.path.join(subdir_path, "train_log.csv")
+        valid_log_path = os.path.join(subdir_path, "valid_log.csv")
+
+        # Check if the files exist
+        if os.path.exists(train_log_path):
+            # Read train_log.csv into a dataframe and add an experiment column
+            train_df = pd.read_csv(train_log_path)
+            train_df['Experiment'] = subdir  # Label each row with the experiment name
+            train_experiment_dfs.append(train_df)
+
+        if os.path.exists(valid_log_path):
+            # Read valid_log.csv into a dataframe and add an experiment column
+            valid_df = pd.read_csv(valid_log_path)
+            valid_df['Experiment'] = subdir  # Label each row with the experiment name
+            val_experiment_dfs.append(valid_df)
+
+# Concatenate all experiment dataframes by columns (parallel storage)
+train_final_df = pd.concat(train_experiment_dfs, axis=1)
+val_final_df = pd.concat(val_experiment_dfs, axis=1)
+
+train_mean_iou = np.mean(train_final_df["iou_score"].values, axis=1)
+val_mean_iou = np.mean(val_final_df["iou_score"].values, axis=1)
+
+train_std_iou = np.std(train_final_df["iou_score"].values, axis=1)
+val_std_iou = np.std(val_final_df["iou_score"].values, axis=1)
+
+# mean_dice = np.mean(final_df["Dice_score"].values, axis=1)
+plot_csv(train_mean_iou, val_mean_iou, train_std_iou, val_std_iou)
