f88e03a4aebf4cf176fe1e13bb0fe61399bda669
Pacific89 <philipp.faulhammer@gmail.com>
2024-02-15 16:04:01 +0100

    class distributions

--- fast_api/ml_api.py
+++ fast_api/ml_api.py
@@ -19,55 +19,88 @@ from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap, BoundaryNorm
 import json
 
-# from config import CONFIG
+from config import CONFIG
 import numpy as np
 
-def update_analysis(id,completed=True):
+# print(CONFIG)
+
+def update_analysis(analysis_id,completed=True, error="", status="none"):
     """update analysis entry in django backend
     """
-    analyses_url = 'http://django:8000/api/v1/analyses/' + str(id) + '/' #localhost:8000 or django:8000 (if using docker)
+    # analyses_url = 'http://django:8000/api/v1/analyses/{0}/'.format(str(analysis_id)) #localhost:8000 or django:8000 (if using docker)
+    analyses_url = 'http://{0}:8000/api/v1/analyses/{1}/'.format(CONFIG["HOST"], str(analysis_id)) #localhost:8000 or django:8000 (if using docker)
 
     payload = {
         "completed" : completed,
-        "end_time" : datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        "errors" : error,
+        "end_time" : datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+        "status" : status,
     }
-    print("Update analysis...")
+    print("sending analysis-PATCH-request...")
     response = requests.patch(analyses_url, data=payload)
-    print(response.text)
     print("done")
 
 def get_image(img_id):
     """
     Get image from django backend
     """
-    print("getting image...")
-    image_url = 'http://django:8000/api/v1/images/{0}/'.format(img_id) #localhost:8000 or django:8000 (if using docker)
+    # image_url = 'http://django:8000/api/v1/images/{0}/'.format(img_id) #localhost:8000 or django:8000 (if using docker)
+    image_url = 'http://{0}:8000/api/v1/images/{1}/'.format(CONFIG["HOST"], img_id) #localhost:8000 or django:8000 (if using docker)
 
     response = requests.get(image_url)
-    print(response.content)
     encoded = response.content
     decoded = json.loads(encoded.decode())
     return decoded
 
-def send_result(img, package):
+def get_class_distributions(img, ontology, ignore_zero=True):
+    """
+    Get class distributions from image
+    if ignore_zero is True, ignore class 0 (Background) and calculate distribution
+    in relation to the sum of all other classes
+    """
+    img = np.array(img)
+
+    if ignore_zero:
+        # denom = np.sum(img[img != 0])
+        print("Ignoring class 0 (Background))")
+        print("sum: ", denom)
+    else:
+        denom = np.sum(img)
+
+    class_distributions = ontology.copy()
+    class_names = list(ontology.keys())
+    unique, counts = np.unique(img, return_counts=True)
+    print(unique)
+    print(counts)
+    denom = np.sum(counts[1:])
+    for i in unique:
+        name = class_names[i]
+        class_distributions[name] = round(counts[i]/denom, 5)
+        print(class_distributions)
+
+    del class_distributions['background']
+    class_distributions = str(class_distributions)
+    print("Class Dist.:")
+    print(class_distributions)
+    return class_distributions
+
+def send_result(color_coded_img, categorically_coded_img, ontology, package):
     """
     Send result to to django backend
     """
-    print("sending request...")
-    mask_api_url = 'http://django:8000/api/v1/masks/' #localhost:8000 or django:8000 (if using docker)
 
     source_model_url = package['model_path']
     parent_file_url = package['file_path']
     parent_img_id = package['parent_img_id']
     ml_model_id = package['ml_model_id']
 
+    class_distributions = get_class_distributions(categorically_coded_img, ontology)
+
     image_data = get_image(parent_img_id)
     dataset = image_data['dataset']
     
     img_byte_arr = io.BytesIO()
-    img.save(img_byte_arr, format='png')
-
-    print(type(img_byte_arr))
+    color_coded_img.save(img_byte_arr, format='png')
 
     payload = {
         "name" : 'test',
@@ -78,19 +111,21 @@ def send_result(img, package):
         "parent_image" : parent_img_id,
         "source_model" : ml_model_id,
         "source_model_url" : source_model_url,
-        "parent_image_url" : parent_file_url
+        "parent_image_url" : parent_file_url,
+        "class_distributions" : class_distributions,
     }
-    file = {'mask': ('image.jpg', img_byte_arr.getvalue(), 'image/png')}    # print(image)
+    file = {'mask': ('image.jpg', img_byte_arr.getvalue(), 'image/png')}
     
+    print("sending mask-POST-request...")
+    mask_api_url = 'http://{0}:8000/api/v1/masks/'.format(CONFIG["HOST"]) #localhost:8000 or django:8000 (if using docker)
     response = requests.post(mask_api_url, data=payload, files=file)
-    print(response.text)
     print("done")
 
     pass
 
 def plot_save_mask(mask, ontology):
 
-    print("plotting results...")
+    print("plot & save results...")
     mask = np.array(mask)
     labels = list(ontology.keys())
     colors_hex = list(ontology.values())
@@ -99,7 +134,6 @@ def plot_save_mask(mask, ontology):
     col = ListedColormap(colors_hex)
     bounds = np.arange(len(colors_hex))
     norm = BoundaryNorm(bounds, col.N)  
-    print(mask.shape)
     # img_arr = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.uint8)
     fig = plt.figure(figsize=mask.shape[:2][::-1], dpi=1)
 
@@ -111,7 +145,6 @@ def plot_save_mask(mask, ontology):
     plt.imshow(mask, cmap=col, interpolation='nearest', norm=norm)
     plt.axis('off')
     
-    print("saving to buffer...")
     io_buf = io.BytesIO()
     fig.savefig(io_buf, format='raw')
     io_buf.seek(0)
@@ -119,8 +152,9 @@ def plot_save_mask(mask, ontology):
                         newshape=(int(fig.bbox.bounds[3]), int(fig.bbox.bounds[2]), -1))
     io_buf.close()
 
-    print("done")
     pil_img = Image.fromarray(img_arr)
+
+    print("done")
     
     return pil_img
 
@@ -170,17 +204,34 @@ def create_trainer_object(model_url):
 def predict(package: dict, input: list) -> np.ndarray:
     """
     """
-    print(package)
     model_url = package['model_path']
     image_url = package['file_path']
     add_parent_dir()
-    print("loading image...")
     img = load_image(image_url)
-    print("Image loaded")
     trainer, ontology = create_trainer_object(model_url)
+    update_analysis(package['analysis_id'], completed=False, status="computing")
     mask_pred = trainer.predict_whole_image(img)
     # mask_pred = img
     color_coded_mask = plot_save_mask(mask_pred, ontology)
-    print("Done")
-    print("FINISHED")
-    return mask_pred, color_coded_mask
+    print("Img: {0} finished".format(image_url))
+    return mask_pred, color_coded_mask, ontology
+
+
+def manage_prediction_request(package: dict):
+
+    try:
+        mask_pred, color_coded_mask, ontology = predict(package=package, input=[])
+        error = False
+        send_result(color_coded_mask, mask_pred, ontology, package)
+        update_analysis(package['analysis_id'], completed=True, status="processed")
+        return {
+            "error": error,
+        }
+    except Exception as e:
+        print("ERROR:")
+        print(e)
+        error = True
+        update_analysis(package['analysis_id'], completed=False, error=e)
+        return {
+        "error": error,
+    }
