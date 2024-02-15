8825f32254723ecfada0be6caadb88a9028cc3d0
Pacific89 <philipp.faulhammer@gmail.com>
2024-02-15 16:04:30 +0100

    use backgroundTasks

--- fast_api/main.py
+++ fast_api/main.py
@@ -8,7 +8,7 @@ from joblib import load
 
 import uvicorn
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, BackgroundTasks
 from fastapi.logger import logger
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import RedirectResponse, JSONResponse
@@ -17,7 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from api_schema import InferenceInput, InferenceResult, InferenceResponse, ErrorResponse
-from ml_api import predict, send_result, update_analysis
+from ml_api import manage_prediction_request
 from config import CONFIG
 from exception_handler import validation_exception_handler, python_exception_handler
 
@@ -45,12 +45,10 @@ app.add_exception_handler(Exception, python_exception_handler)
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Load the ML model
-    print("lifespan start")
     logger.info('Running envirnoment: {}'.format(CONFIG['ENV']))
     logger.info('PyTorch using device: {}'.format(CONFIG['DEVICE']))
     yield
     # Clean up the ML models and release the resources
-    print("lifespan end")
 
 
 @app.post('/api/v1/predict',
@@ -58,17 +56,13 @@ async def lifespan(app: FastAPI):
           responses={422: {"model": ErrorResponse},
                      500: {"model": ErrorResponse}}
           )
-def do_predict(request: Request, body: InferenceInput):
+async def do_predict(request: Request, body: InferenceInput, background_tasks: BackgroundTasks):
     """
     Perform prediction on input data:
     file_path: str = Field(..., example='12/images/img_1.jpg', title='Path to the input image file')
     model_path: str = Field(..., example='12/models/model_1.pt', title='Path to the model file')
     """
-    error = False
-    completed = False
-    mask_pred = None
-    results_file_name = "mask_pred.jpg"
-    # results = {"mask" : "mask_pred"}
+
     package = {
         "model_path": body.model_path,
         "file_path": body.file_path,
@@ -76,22 +70,14 @@ def do_predict(request: Request, body: InferenceInput):
         "parent_img_id": body.parent_img_id,
         "ml_model_id": body.ml_model_id
     }
+
     try:
-        mask_pred, color_coded_mask = predict(package=package, input=[])
-        print("prediction function left...")
-        send_result(color_coded_mask, package)
-        update_analysis(package['analysis_id'], completed=True)
-        return {
-            "error": error,
-        }
+        background_tasks.add_task(manage_prediction_request, package)
+        return {"error": False}
     except Exception as e:
-        print("ERROR:")
         print(e)
-        error = True
-        update_analysis(package['analysis_id'], completed=False)
-        return {
-        "error": error,
-    }
+        return {"error": True}
+
 
 
 
